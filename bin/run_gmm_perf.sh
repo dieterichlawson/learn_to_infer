37d62b7848b710dfa5287b46e803bfba451457d9
Dieterich Lawson <dieterich.lawson@gmail.com>
2021-02-09 14:05:17 -0800

    Updates to data_dim sweep config

--- bin/run_gmm_perf.sh
+++ bin/run_gmm_perf.sh
@@ -64,9 +64,30 @@
 #  --logdir=gs://l2i/data_dim_sweep 
 
 
+#python3 run_gmm.py \
+#  --model_name="mean_scale_weight" \
+#  --data_dim=128 \
+#  --num_encoders=2 \
+#  --num_decoders=2 \
+#  --num_heads=16 \
+#  --key_dim=32 \
+#  --value_dim_per_head=64 \
+#  --min_k=3 \
+#  --max_k=3 \
+#  --data_points_per_mode=50 \
+#  --cov_dof=134 \
+#  --separation_multiplier=2. \
+#  --batch_size=64 \
+#  --eval_batch_size=256 \
+#  --lr=1e-3 \
+#  --checkpoint_every=2500 \
+#  --summarize_every=2500 \
+#  --num_steps=100000000 \
+#  --logdir=gs://l2i/data_dim_sweep 
+
 python3 run_gmm.py \
   --model_name="mean_scale_weight" \
-  --data_dim=128 \
+  --data_dim=256 \
   --num_encoders=2 \
   --num_decoders=2 \
   --num_heads=16 \
@@ -75,7 +96,7 @@ python3 run_gmm.py \
   --min_k=3 \
   --max_k=3 \
   --data_points_per_mode=50 \
-  --cov_dof=34 \
+  --cov_dof=260 \
   --separation_multiplier=2. \
   --batch_size=64 \
   --eval_batch_size=256 \
